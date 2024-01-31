4092d0086578411a8a8d0eddcb41b33eaac57111
GBalatti9 <gas.balatti@gmail.com>
2024-01-31 11:06:29 -0300

    Card component is gonna contain the table and the form

---
+++ src/components/Card.svelte
@@ -0,0 +1,16 @@
+
+
+<table>
+    <tbody>
+        <tr>
+            <td colspan="5">hola</td>
+        </tr>
+        <tr>
+            <th>#</th>
+            <th>Task</th>
+            <th>Status</th>
+            <th>Edit</th>
+            <th>Delete</th>
+        </tr>
+    </tbody>
+</table>
