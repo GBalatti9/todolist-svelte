cf28f23f1de357bcb73e19e89b215ccf4fd7449f
GBalatti9 <gas.balatti@gmail.com>
2024-01-31 11:26:38 -0300

    Form created and setting up the logic of the component

--- src/components/Form.svelte
+++ src/components/Form.svelte
@@ -1,7 +1,62 @@
 
+<script>
+    import { createEventDispatcher } from "svelte";
 
-<form>
+    const dispatch = createEventDispatcher();
+
+    const status = ['Done', 'In Progress', 'To Start'];
+
+    const formData = {
+        id: 0,
+        task: '',
+        status: 'Status',
+        editing: false,
+    }
+
+    let error = '';
+    $: noDisplay = error === '';
+
+    const handleSubmit = () => {
+        if (formData.task.trim().length === 0)   return error = 'Cannot set empty task';
+        if (formData.status.trim() === 'Status') return error = 'Change the status, please';
+
+        error = '';
+        dispatch('add-task', formData);
+    }
+
+</script>
+
+<form on:submit|preventDefault = { handleSubmit }>
     <fieldset>
         <legend>Add a new task</legend>
+        
+        <input type="task" placeholder="Task..." bind:value = { formData.task }>
+
+        <select name="status" id="status"        bind:value = { formData.status }>
+            <option value="Status" disabled>Status</option>
+            { #each status as item }
+                <option value={ item }> { item } </option>
+            { /each }
+        </select>
+
+        <button type="submit"> New Task </button>
+        
+        <p class:noDisplay class="red">{ error }</p>
     </fieldset>
 </form>
+
+<style>
+
+    .noDisplay{
+        display: none;
+    }
+
+    .red{
+        color: red;
+    }
+
+    button {
+        cursor: pointer;
+    }
+
+</style>
