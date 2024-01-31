a72ddcbc0059d3aa6c4145a9614fc8b588a28175
GBalatti9 <gas.balatti@gmail.com>
2024-01-31 12:49:23 -0300

    Adding save button to save changes

--- src/components/TableRows.svelte
+++ src/components/TableRows.svelte
@@ -2,11 +2,6 @@
     export let id, task, status, editing, handleDelete, handleEdit, closeEditing, handleInputChange, save;
     const statusList = ['Done', 'In Progress', 'To Start'];
 
-    let formEdited = {
-        task,
-        status
-    }
-
     let taskInput;
     $: if ( taskInput ) {
         taskInput.focus();
@@ -41,6 +36,20 @@
     {/if}
     { #if editing }
         <td>
+            <button on:click={ save }>
+                <svg 
+                xmlns        = "http://www.w3.org/2000/svg" 
+                fill         = "none" 
+                viewBox      = "0 0 24 24" 
+                stroke-width = "1.5" 
+                stroke       = "currentColor" 
+                class        = "w-6 h-6">
+                    <path 
+                    stroke-linecap  = "round" 
+                    stroke-linejoin = "round" 
+                    d               = "M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z" />
+                </svg>
+            </button>
             <button on:click = { closeEditing }>
                 <svg 
                 xmlns        = "http://www.w3.org/2000/svg" 
@@ -54,9 +63,8 @@
                     stroke-linejoin = "round" 
                     d               = "M6 18 18 6M6 6l12 12" />
                 </svg>
-
+                
             </button>
-            <button on:click={ save }>Save</button>
         </td>
     { :else }
     <td>
