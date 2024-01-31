a5aa853776d1792b0ade84f0d3631b9e41090328
GBalatti9 <gas.balatti@gmail.com>
2024-01-31 11:08:10 -0300

    First styles to the table

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -14,3 +14,24 @@
         </tr>
     </tbody>
 </table>
+
+<style>
+    table{
+        border-collapse: collapse;
+        width: 100%;
+    }
+    th,
+    :global(td) {
+        border: 1px solid #ddd;
+        padding: 8px;
+        text-align: center;
+    }
+
+    :global(td) {
+        width: 100px;
+    }
+
+    th {
+        background-color: #f2f2f2;
+    }
+</style>
